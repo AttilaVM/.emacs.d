ad7445cec0096db701400a1b82d4e8e96911f6a8
Attila Molnar <ate.molnar2@gmail.com>
2016-02-02 17:28:52 +0100

    Using .fonts instead of .font, font cache update

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 inconsolataUrl="http://www.fontsquirrel.com/fonts/download/Inconsolata"
 
-if ! [ -d ~/.font ]; then
-    mkdir ~/.font
+if ! [ -d ~/.fonts ]; then
+    mkdir ~/.fonts
 fi
 
-cd ~/.font/
+cd ~/.fonts/
 
 wget $inconsolataUrl -O inconsolata.zip
 
 7z e inconsolata.zip
+
+fc-cache -rv
