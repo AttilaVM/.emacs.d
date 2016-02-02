0ad3d615d5f83facc6cf23ccc78c5ccc20574752
Attila Molnar <ate.molnar2@gmail.com>
2016-02-02 15:25:27 +0100

    Basic helm configuration
    Configured features
               file finding
               buffer finding
               bookmarking
               helm clipboard
               gitting

--- init.el
+++ init.el
@@ -64,8 +64,8 @@
 ;;-----------------Global Function------------------
 
 ;; Interactive mode for easier file and buffer navigation
-(require 'ido)
-(ido-mode t)
+;; (require 'ido)
+;; (ido-mode t)
 
 ;; Spell checking in comments for different modes
 (dolist (hook '(lisp-mode-hook
@@ -104,6 +104,53 @@
 
 ;;Auto-complete is a dependency of yasnipper
 (package-initialize)
+
+;; Helm
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+;; Use helm for file finding
+(global-set-key (kbd "C-x c-f") 'helm-find-files)
+
+;; Use helm-buffers-list instead of default helm buffer lister
+(global-set-key (kbd "s-x b") 'helm-buffers-list)
+
+;; get the list of the bookmarks (C-x r m for saving bookmarks) 
+(global-set-key (kbd "C-x r r") 'helm-bookmarks)
+
+;; More easier way to acces Emacs's internal "clipboard" 
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+;; Use helm with isearch 
+(global-set-key (kbd "s-s") 'helm-occur-from-isearch)
+
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+;; Use helm to explore a git project-am
+(require 'helm-ls-git)
+(global-set-key (kbd "C-<f6>") 'helm-browse-project)
+
+(helm-mode 1)
+
 (require 'auto-complete)
 
 ;; Rainbow parenthese
