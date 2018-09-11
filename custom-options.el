aca25a3f8a3daa79eda71ed23869a93a7ad76891
Attila Molnar <ate.molnar2@gmail.com>
2018-09-11 17:20:41 +0200

    Add: custom options file

---
+++ custom-options.el
@@ -0,0 +1,104 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "dim gray")
+ '(company-quickhelp-color-foreground "gainsboro")
+ '(company-quickhelp-use-propertized-text t)
+ '(dired-filter-saved-filters
+	 (quote
+		(("disk-images"
+			(or
+			 (extension . "iso")
+			 (extension . "img")
+			 (extension . "raw")))
+		 ("archives"
+			(or
+			 (extension . "zip")
+			 (extension . "7z")
+			 (extension . "tar.gz")
+			 (extension . "tar.xz")
+			 (extension . "tar.bz")
+			 (extension . "tar.bz")
+			 (extension . "rar")))
+		 ("sound"
+			(or
+			 (extension . "flac")
+			 (extension . "ogg")
+			 (extension . "wav")
+			 (extension . "mp3")))
+		 ("video"
+			(or
+			 (extension . "mkv")
+			 (extension . "ogv")
+			 (extension . "mp4")
+			 (extension . "avi")))
+		 ("code"
+			(or
+			 (extension . "clj")
+			 (extension . "cljs")
+			 (extension . "cljc")
+			 (extension . "py")
+			 (extension . "rb")
+			 (extension . "r")
+			 (extension . "js")
+			 (extension . "elm")
+			 (extension . "hs")
+			 (extension . "nix")
+			 (extension . "h")
+			 (extension . "c")
+			 (extension . "cpp")
+			 (extension . "java")
+			 (extension . "go")
+			 (extension . "sh")
+			 (extension . "php")))
+		 ("documents"
+			(or
+			 (extension . "txt")
+			 (extension . "rtd")
+			 (extension . "org")
+			 (extension . "md")
+			 (extension . "markdown")
+			 (extension . "eldoc")
+			 (extension . "tex")
+			 (extension . "bib")
+			 (extension . "odt")
+			 (extension . "odc")
+			 (extension . "odp")
+			 (extension . "doc")
+			 (extension . "docx")
+			 (extension . "xls")
+			 (extension . "xlsx")
+			 (extension . "ppt")
+			 (extension . "pptx")
+			 (extension . "epub")
+			 (extension . "djv")
+			 (extension . "djvu")
+			 (extension . "pdf")))
+		 ("images"
+			(or
+			 (extension . "png")
+			 (extension . "jpg")
+			 (extension . "jpeg")
+			 (extension . "tif")
+			 (extension . "tiff")
+			 (extension . "gif")
+			 (extension . "exr")
+			 (extension . "svg")
+			 (extension . "cr2")))
+		 ("backup"
+			(or
+			 (regexp . ".bck$")
+			 (regexp . "~$")
+			 (regexp . "-bck$"))))))
+ '(safe-local-variable-values
+	 (quote
+		((org-babel-temporary-directory . "/home/attila/projects/analogika-cms")
+		 (org-babel-temporary-directory . "/home/attila/projects/analogika-cms/.dir-locals.el")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
