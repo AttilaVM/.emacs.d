ba002d6e4e8caf1c3170aecf9723d7e133b6fc3a
Attila Molnar <ate.molnar2@gmail.com>
2018-09-14 19:31:21 +0200

    Default PDF latex classes Sane comint mode nav hotkeys

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -12,3 +11,100 @@
 (if user-emacs-config-file
 		(load user-emacs-config-file)
 	(load "~/.emacs.d/init-old.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dired-filter-saved-filters
+	 (quote
+		(("disk-images"
+			(or
+			 (extension . "iso")
+			 (extension . "img")
+			 (extension . "raw")))
+		 ("archives"
+			(or
+			 (extension . "zip")
+			 (extension . "7z")
+			 (extension . "tar.gz")
+			 (extension . "tar.xz")
+			 (extension . "tar.bz")
+			 (extension . "tar.bz")
+			 (extension . "rar")))
+		 ("sound"
+			(or
+			 (extension . "flac")
+			 (extension . "ogg")
+			 (extension . "wav")
+			 (extension . "mp3")))
+		 ("video"
+			(or
+			 (extension . "mkv")
+			 (extension . "ogv")
+			 (extension . "mp4")
+			 (extension . "avi")))
+		 ("code"
+			(or
+			 (extension . "clj")
+			 (extension . "cljs")
+			 (extension . "cljc")
+			 (extension . "py")
+			 (extension . "rb")
+			 (extension . "r")
+			 (extension . "js")
+			 (extension . "elm")
+			 (extension . "hs")
+			 (extension . "nix")
+			 (extension . "h")
+			 (extension . "c")
+			 (extension . "cpp")
+			 (extension . "java")
+			 (extension . "go")
+			 (extension . "sh")
+			 (extension . "php")))
+		 ("documents"
+			(or
+			 (extension . "txt")
+			 (extension . "rtd")
+			 (extension . "org")
+			 (extension . "md")
+			 (extension . "markdown")
+			 (extension . "eldoc")
+			 (extension . "tex")
+			 (extension . "bib")
+			 (extension . "odt")
+			 (extension . "odc")
+			 (extension . "odp")
+			 (extension . "doc")
+			 (extension . "docx")
+			 (extension . "xls")
+			 (extension . "xlsx")
+			 (extension . "ppt")
+			 (extension . "pptx")
+			 (extension . "epub")
+			 (extension . "djv")
+			 (extension . "djvu")
+			 (extension . "pdf")))
+		 ("images"
+			(or
+			 (extension . "png")
+			 (extension . "jpg")
+			 (extension . "jpeg")
+			 (extension . "tif")
+			 (extension . "tiff")
+			 (extension . "gif")
+			 (extension . "exr")
+			 (extension . "svg")
+			 (extension . "cr2")))
+		 ("backup"
+			(or
+			 (regexp . ".bck$")
+			 (regexp . "~$")
+			 (regexp . "-bck$")))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
